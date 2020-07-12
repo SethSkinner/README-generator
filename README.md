7aac721e2df742ff147dadab9e3021d9e9306a68
Seth Skinner <sethwskinner@gmail.com>
2020-07-11 21:07:35 -0500

    finishing up application

---
+++ README.md
@@ -0,0 +1,40 @@
+# README generator 
+## Description of App: 
+ >this project will make a readme based on users input
+  ## Table of contents: 
+ this
+ *  read
+ *  me
+ *  has
+ *  lots
+ *  of
+ *  content! 
+## Installation Information: 
+ npm i when you first cd into the file to download packages needed 
+## User Story: 
+ this app is used to generate a readme for github repos 
+## License: MIT 
+## Contributions: 
+ Seth Skinner 
+## Tests: 
+ You can test this app by testing the prompts 
+### Github Info: 
+ @SETHSKINNER 
+ *  github.io/SethSkinner 
+# README generator 
+## Description of App: 
+ >This project will allow a user to create a readme based on user input to prompts using inquirer
+  ## Table of contents: 
+  
+## Installation Information: 
+ npm i install all of the json packages included in the repo 
+## User Story: 
+ this app will be used by the user to create a professional readme with a command line app built in node.js 
+## License: MIT 
+## Contributions: 
+ Seth Wyatt Skinner 
+## Tests: 
+ You can test the app by righting a function to test the appending of files into the readme 
+### Github Info: 
+ @SethSkinner 
+ *  https://github.com/SethSkinner/README-generator 
--- index.js
+++ index.js
@@ -1,68 +1,68 @@
 //variable requiring inquirer for this file
-var inquirer = require("inquirer");
+var inquirer = require('inquirer');
 //variable for fs to make things asynchronys 
-var fs = require("fs");
+var fs = require('fs');
 
 //arrays to hold prompt questions for user input 
 var prompts = [
   {
-    type: "input",
-    name: "title",
-    message: "What is your the name of your Project?",
+    type: 'input',
+    name: 'title',
+    message: 'What would you like to name the project?',
   },
   {
-    type: "input",
-    name: "description",
-    message: "Describe your project",
+    type: 'input',
+    name: 'description',
+    message: 'How would you describe the project?',
   },
   {
-    type: "input",
-    name: "contents",
+    type: 'input',
+    name: 'contents',
     message:
-      "List the content of your README.md? (start each list item with a space)",
+      'List the content of the readme with a space after each',
   },
   {
-    type: "input",
-    name: "installation",
-    message: "Instalation Steps",
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation steps for the project',
   },
   {
-    type: "input",
-    name: "userstory",
-    message: "What is the use for your App?",
+    type: 'input',
+    name: 'userstory',
+    message: 'What is the user story for the application?',
   },
   {
-    type: "list",
-    name: "license",
+    type: 'list',
+    name: 'license',
     choices: [
-      "Apache 2.0",
-      "GPL",
-      "LGPL",
-      "MIT",
-      "Mozilla Public License 2.0",
-      "Common Development and Distribution License",
-      "Eclipse Public License version 2.0",
+      'Apache 2.0',
+      'GPL',
+      'LGPL',
+      'MIT',
+      'Mozilla Public License 2.0',
+      'Common Development and Distribution License',
+      'Eclipse Public License version 2.0',
     ],
   },
   {
-    type: "input",
-    name: "contributors",
-    message: "Who made the application?",
+    type: 'input',
+    name: 'contributors',
+    message: 'Who created the project?',
   },
   {
-    type: "input",
-    name: "tests",
-    message: "How can you test your app?",
+    type: 'input',
+    name: 'tests',
+    message: 'What are ways that you can test the app?',
   },
   {
-    type: "input",
-    name: "githubName",
-    message: "What is your Git Hub username?",
+    type: 'input',
+    name: 'githubName',
+    message: 'What is youre github name?',
   },
   {
-    type: "input",
-    name: "githubRepoName",
-    message: "What is the link to your Git Hub Repository?",
+    type: 'input',
+    name: 'githubRepoName',
+    message: 'What is the link to your Git Hub Repository?',
   },
 ];
 
@@ -71,34 +71,34 @@ function Start() {
   //starts asking prompted questions to user
   inquirer.prompt(prompts).then((data) => {
     //takes content input and splits it into seperate strings and then adds them to a new line
-    var contents = data.contents.split(" ").join("\n *  ");
+    var contents = data.contents.split(' ').join('\n *  ');
 
     //appends title into file page
-    fs.appendFileSync("README.md", `# ${data.title} \n`);
+    fs.appendFileSync('README.md', `# ${data.title} \n`);
 
     //appends the users decription into the file page
-    fs.appendFileSync("README.md", `## Description of App: \n >${data.description}\n  `);
+    fs.appendFileSync('README.md', `## Description of App: \n >${data.description}\n  `);
 
     //appends the contents inputted by user into the file page
-    fs.appendFileSync("README.md", `## Table of contents: \n ${contents} \n`);
+    fs.appendFileSync('README.md', `## Table of contents: \n ${contents} \n`);
 
     //appends installation instructions into the file page
-    fs.appendFileSync("README.md", `## Installation Information: \n ${data.installation} \n`);
+    fs.appendFileSync('README.md', `## Installation Information: \n ${data.installation} \n`);
 
     //appends the user story into the file page
-    fs.appendFileSync("README.md", `## User Story: \n ${data.userstory} \n`);
+    fs.appendFileSync('README.md', `## User Story: \n ${data.userstory} \n`);
 
     //appends the license information into the file page
-    fs.appendFileSync("README.md", `## License: ${data.license} \n`);
+    fs.appendFileSync('README.md', `## License: ${data.license} \n`);
 
     //appends the contributions into the file page
-    fs.appendFileSync("README.md", `## Contributions: \n ${data.contributors} \n`);
+    fs.appendFileSync('README.md', `## Contributions: \n ${data.contributors} \n`);
 
     //appends the tests into the file page
-    fs.appendFileSync("README.md", `## Tests: \n ${data.tests} \n`);
+    fs.appendFileSync('README.md', `## Tests: \n ${data.tests} \n`);
 
     //appends the github infomation of the user into the file page
-    fs.appendFileSync("README.md", `### Github Info: \n ${data.githubName} \n *  ${data.githubRepoName} \n` );
+    fs.appendFileSync('README.md', `### Github Info: \n ${data.githubName} \n *  ${data.githubRepoName} \n` );
   });
 }
 
